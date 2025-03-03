1bd0bf2f6f25344f9d0408fdfa66236926d32dc7
ry <97790074+YYReese@users.noreply.github.com>
2025-03-03 16:40:10 +0000

    comment

--- sort.py
+++ sort.py
@@ -7,6 +7,7 @@ def pivot_sort(arr):
     # - right: elements greater than or equal to the pivot
     left = [x for x in arr[1:] if x < pivot]
     right = [x for x in arr[1:] if x >= pivot]
+    # little changes
     
     # Recursively sort the subarrays and concatenate them with the 
     return pivot_sort(left) + [pivot] + pivot_sort(right)
